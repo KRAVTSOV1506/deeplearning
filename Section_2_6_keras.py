8adb507b56e12811589421c737d1830a74428e77
Kravtsov Dmitriy <kravtsov.dmitriy1506@yandex.ru>
2018-11-21 22:58:32 +0300

    Full Keras 2_6

--- Section_2_6_keras.py
+++ Section_2_6_keras.py
@@ -2,10 +2,29 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Input, Dense, Activation
 
-#модель будет создаваться последовательно слой за слоем
+# модель будет создаваться последовательно слой за слоем
 logr = Sequential()
-#добавляем один плотный слой, входы которого будут размерности два,
+# добавляем один плотный слой, входы которого будут размерности два,
 # а на выходе будет логистический сигмоид от входов
-logr.add(Dense(1, input_dim=2, activation= ' sigmoid ' ))
-#модель собственно компилируется с заданной целевой функцией и метрикой точности
-logr.compile(loss= ' binary_crossentropy ' , optimizer= ' sgd ' , metrics=[ ' accuracy ' ])
+logr.add(Dense(1, input_dim=2, activation= 'sigmoid' ))
+# модель компилируется с заданной целевой функцией и метрикой точности
+logr.compile(loss= 'binary_crossentropy' , optimizer= 'sgd' , metrics=[ 'accuracy' ])
+
+def sampler(n, x, y):
+    return np.random.normal(size=[n, 2]) + [x, y]
+
+# генерируем два двумерных нормальных распределения, одно с центром в
+# точке (−1; −1), другое в точке (1; 1), с дисперсией 1 по обоим компонентам
+def sample_data(n=1000, p0=(-1., -1.), p1=(1., 1.)):
+    zeros, ones = np.zeros((n, 1)), np.ones((n, 1))
+    labels = np.vstack([zeros, ones])
+    z_sample = sampler(n, x=p0[0], y=p0[1])
+    o_sample = sampler(n, x=p1[0], y=p1[1])
+    return np.vstack([z_sample, o_sample]), labels
+
+X_train, Y_train = sample_data()
+X_test, Y_test = sample_data(100)
+
+#задаем тренировочное множество, число эпох и размер мини-батча
+logr.fit(X_train, Y_train, batch_size=16, nb_epoch=100,
+    verbose=1, validation_data=(X_test, Y_test))
